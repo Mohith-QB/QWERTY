508ed426fb7f508fbf1836957f309e53ebc33f33
“Mohith” <mohith.nagirimadugu@qburst.com>
2024-10-09 15:16:43 +0530

    Add Numbers

---
+++ Add.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the First number: "))
+num2  = int(input("Enter the Second number: "))
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
