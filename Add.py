1082e0da90d70ed5c270c7bdb5067537c1820866
Mohith-QB <mohith.nagirimadugu@qburst.com>
2024-10-09 16:24:03 +0530

    Update Add.py

--- Add.py
+++ Add.py
@@ -6,4 +6,13 @@ sum = num1 + num2
 
 # printing values
 print("Sum of", num1, "and", num2 , "is", sum)
-uhfgjfjdg
+
+num3 = int(input("Enter the First number: "))
+num4  = int(input("Enter the Second number: "))
+
+# Adding two nos
+sub = num3 - num4
+
+# printing values
+print("Sub of", num3, "and", num4 , "is", sub)
+
